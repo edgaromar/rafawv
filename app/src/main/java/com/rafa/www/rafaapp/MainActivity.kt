5bb96050fd4e48e48d1b195cf6197c0ed51efcb2
Edgar <dynamoman@gmail.com>
2019-08-14 21:45:55 -0500

    gyhjk

--- app/src/main/java/com/rafa/www/rafaapp/MainActivity.kt
+++ app/src/main/java/com/rafa/www/rafaapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.rafa.www.rafaapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainL.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+
 
         val webV = webView
 
